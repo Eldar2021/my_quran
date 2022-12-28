52173b7f4d952da0d1677d3a02fe8744bf1b09c5
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-12-28 18:19:50 +0600

    developers page updated

--- lib/modules/settings/view/developers_view.dart
+++ lib/modules/settings/view/developers_view.dart
@@ -36,19 +36,23 @@ class DevelopersView extends StatelessWidget {
               style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
                 ),
+                maximumSize: Size(240, MediaQuery.of(context).size.height * 0.06),
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Assets.icons.github.svg(
                     height: MediaQuery.of(context).size.height * 0.06,
-                    color: Theme.of(context).colorScheme.primary,
+                    color: Theme.of(context).colorScheme.onBackground,
                   ),
                   Text(
                     'GitHub',
                     style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
+                      color: Theme.of(context).colorScheme.onBackground,
                       fontSize: MediaQuery.of(context).size.height * 0.04,
                       fontWeight: FontWeight.bold,
                     ),
