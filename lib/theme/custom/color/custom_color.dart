9f2c1e5e7592aa63b671928135bfccda080c6509
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2023-01-07 00:48:22 +0600

    theme colors updated

--- lib/theme/custom/color/custom_color.dart
+++ lib/theme/custom/color/custom_color.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 
 Map<int, Color> targetColors = {
-  1: Colors.blue,
-  2: Colors.red,
-  3: Colors.deepOrange,
-  4: Colors.yellow,
-  5: Colors.brown,
-  6: Colors.blueAccent,
-  7: Colors.deepOrangeAccent,
-  8: Colors.deepPurple,
-  9: Colors.deepPurpleAccent,
-  10: Colors.green,
-  11: Colors.indigoAccent,
-  12: Colors.lime,
-  13: Colors.pink,
-  14: Colors.teal,
-  15: Colors.redAccent,
+  0: Colors.red,
+  1: Colors.yellow,
+  2: Colors.green,
+  3: Colors.blue,
+  4: Colors.purple,
 };
