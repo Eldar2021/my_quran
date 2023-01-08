6899019ab5ef3a416342540fd6cafcc5d0b16327
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2023-01-08 17:57:04 +0600

    about us text

--- lib/modules/settings/view/about_us.dart
+++ lib/modules/settings/view/about_us.dart
@@ -20,7 +20,7 @@ class AboutUsVuew extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           const Text(
-            '\t\t\t\tLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\n \t\t It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing \n\n \t\t Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            '\t\t\t\t"Пайгамбарыбыз Мухаммед Мустафа (сав) бизге калтырган эң чон аманат бул Куран. Мусулмандар бир өрөөндө, Куран башка бир өрөөндө калган азыркы доордо бардык дин бир туугандарыбызды куттуу Курандын кереметтүү дүйнөсүнө чакырууну туура көрдүк.\t\t \n\n Улутуңуз, жашаган жериңиз, жынысыңыз, социалдык статусуңуз эмне болсо болсун, жалпы дүйнөлүк Куран хатимине катышып, жамааттагы берекелүү соопко сиз да ортоктош болуңуз”."Эй мусулмандар! \t\t \n\n Аллахтын китеби Куранды бары-жогуңар менен кармангыла! Эч качан бөлүнүп-жарылбагыла!" (Али Имран 103)',
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
           ),
         ],
