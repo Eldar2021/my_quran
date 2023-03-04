e5cfb7eb5dd5d4f5e4786b12d26af2dbfb20021d
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-03-04 15:24:12 +0600

    created mq_storage

--- packages/mq_storage/lib/mq_storage.dart
+++ packages/mq_storage/lib/mq_storage.dart
@@ -1,4 +1,8 @@
 /// My Quran Storage
 library mq_storage;
 
-export 'src/mq_storage.dart';
+export 'src/interface/storage_exception.dart';
+export 'src/interface/storage_interface.dart';
+export 'src/interface/storage_sync_read_interface.dart';
+export 'src/preferences_storage.dart';
+export 'src/secure_storage.dart';
