c66f1a08cb6634ff582592b74ac5bc6f2225c2eb
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-08-03 10:55:06 +0600

    try remove get_it

--- app/lib/app/view/app_view.dart
+++ app/lib/app/view/app_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:mq_storage/mq_storage.dart';
 
 import 'package:my_quran/app/app.dart';
+import 'package:my_quran/core/core.dart';
 import 'package:my_quran/l10n/l10.dart';
-import 'package:my_quran/locator.dart';
 import 'package:my_quran/modules/modules.dart';
 
 class MyApp extends StatelessWidget {
@@ -14,9 +15,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => AppCubit(sl<AppService>(), sl<ThemeService>())),
-        BlocProvider(create: (context) => AuthCubit(sl<AuthService>())),
-        BlocProvider(create: (context) => HomeCubit(sl<HomeService>())),
+        BlocProvider(
+          create: (context) => AppCubit(
+            AppService(context.read<PreferencesStorage>()),
+            ThemeService(context.read<PreferencesStorage>()),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => AuthCubit(AuthService(context.read<PreferencesStorage>(), context.read<RemoteClient>())),
+        ),
+        BlocProvider(
+          create: (context) => HomeCubit(HomeService(context.read<PreferencesStorage>(), context.read<RemoteClient>())),
+        ),
         BlocProvider(create: (context) => QuranAudioCubit(AudioPlayer())),
       ],
       child: const QuranApp(),
