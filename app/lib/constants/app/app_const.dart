58dda9b650a86edca41a77d974e35db2312341c2
Aidaiym <omurbekovnaaidaiym@gmail.com>
2023-11-28 21:28:03 -0800

    fix Analyze code and Unit test  error

--- app/lib/constants/app/app_const.dart
+++ app/lib/constants/app/app_const.dart
@@ -7,7 +7,7 @@ class AppConst {
   static const bismallah = 'بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ';
   static const sajdaSymbol = '۩';
   static const sajdaAyats = [1160, 1722, 1951, 2138, 2308, 2613, 2672, 2915, 3185, 3518, 3994, 4256, 4846, 5905, 6125];
-    
+
   /* 
   yes - https://api.quran.com/api/v4/quran/verses/uthmanit?page_number=1 has sajda symbol ۩
   correct - check from mushaf
