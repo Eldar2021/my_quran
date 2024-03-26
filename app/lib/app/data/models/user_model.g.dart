9f7a470797291f2a73079ec22b725ee0f3989207
Aidaiym <omurbekovnaaidaiym@gmail.com>
2024-03-26 15:38:29 -0700

    run melos format

--- app/lib/app/data/models/user_model.g.dart
+++ app/lib/app/data/models/user_model.g.dart
@@ -6,15 +6,13 @@ part of 'user_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-UserModelResponse _$UserModelResponseFromJson(Map<String, dynamic> json) =>
-    UserModelResponse(
+UserModelResponse _$UserModelResponseFromJson(Map<String, dynamic> json) => UserModelResponse(
       accessToken: json['accessToken'] as String,
       username: json['username'] as String,
       gender: $enumDecodeNullable(_$GenderEnumMap, json['gender']),
     );
 
-Map<String, dynamic> _$UserModelResponseToJson(UserModelResponse instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$UserModelResponseToJson(UserModelResponse instance) => <String, dynamic>{
       'accessToken': instance.accessToken,
       'username': instance.username,
       'gender': _$GenderEnumMap[instance.gender],
