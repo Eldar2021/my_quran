4e39efb74c581ce8aea23f2ea02c6d128aa3afd7
Aidaiym <omurbekovnaaidaiym@gmail.com>
2024-03-27 12:59:02 -0700

    change login architecture

--- app/lib/modules/login/login.dart
+++ app/lib/modules/login/login.dart
@@ -1,4 +1,1 @@
-export 'logic/login_cubit.dart';
-export 'view/login_view.dart';
-export 'view/sign_in_view.dart';
-export 'widget/widgets.dart';
+export 'presentation/presentation.dart';
---
+++ app/lib/modules/login/presentation/presentation.dart
@@ -0,0 +1,4 @@
+export 'cubit/login_cubit.dart';
+export 'view/login_view.dart';
+export 'view/sign_in_view.dart';
+export 'widget/widgets.dart';
