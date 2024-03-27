caaf10b8042d3e33454b701edc427c638060acbd
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-03-27 20:48:01 +0600

    change widget folder name

--- app/lib/modules/login/presentation/presentation.dart
+++ app/lib/modules/login/presentation/presentation.dart
@@ -1,4 +1,5 @@
 export 'cubit/login_cubit.dart';
 export 'view/login_view.dart';
 export 'view/sign_in_view.dart';
-export 'widget/widgets.dart';
+export 'widgets/select_gender.dart';
+export 'widgets/select_lang.dart';
