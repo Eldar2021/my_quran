4e3ca0c4a2bf80d404cc0ea5cda65a4a11fe90ca
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-06-23 14:09:04 +0600

    Deleted Unused coded

--- app/lib/modules/hatim/hatim.dart
+++ app/lib/modules/hatim/hatim.dart
@@ -1,6 +1,3 @@
-export 'presentation/cubit/hatim_juz/hatim_juz_cubit.dart';
-export 'presentation/cubit/hatim_pages/hatim_pages_cubit.dart';
-export 'presentation/cubit/hatim_read/hatim_read_cubit.dart';
 export 'presentation/presentation.dart';
 export 'domain/domain.dart';
 export 'data/data.dart';
--- app/lib/modules/hatim/presentation/presentation.dart
+++ app/lib/modules/hatim/presentation/presentation.dart
@@ -1,7 +1,6 @@
 export 'widgets/hatim_page_status_card.dart';
 export 'widgets/juz_percent_widget.dart';
 export 'widgets/vertical_text.dart';
-export 'view1/hatim_select_page_view.dart';
-export 'view1/hatim_view.dart';
-export 'cubit/hatim_juzs/hatim_juzs_cubit.dart';
+export 'view/hatim_select_page_view.dart';
+export 'view/hatim_view.dart';
 export 'bloc/hatim_bloc.dart';
