8a634d54d2aa1b2d3cebfe0a6acd8419ed19dcb5
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-08-12 21:19:30 +0600

    Writed tests

--- packages/mq_remote_client/test/mq_remote_client_test.dart
+++ packages/mq_remote_client/test/mq_remote_client_test.dart
@@ -1,232 +1,79 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:mq_either/mq_either.dart';
 import 'package:mq_remote_client/mq_remote_client.dart';
 
-class MockClient extends Mock implements MqRemoteClient {}
+import 'mock_network_client.dart';
+import 'test_model.dart';
 
 void main() {
-  late MockClient client;
-  const mockUrl = 'https://jsonplaceholder.typicode.com/posts';
-  const mockValue = 'mock-value';
+  late MqRemoteClient client;
 
-  final requestBody = {'key': 'value'};
-  final mockResponse = {'responseKey': 'responseValue'};
+  const getListUrl = 'https://encointer.github.io/feed/community_messages/en/cm.json';
+  const getUrl = 'https://eldar2021.github.io/encointer/test_data.json';
+  const emptyList = 'https://eldar2021.github.io/encointer/test/empty_list.json';
 
-  setUp(() {
-    client = MockClient();
-  });
+  setUp(
+    () => client = MqRemoteClient(
+      dio: Dio(),
+      network: MockNetworkClient(),
+    ),
+  );
 
-  group('GET requests', () {
-    test('Success', () async {
-      when(() => client.get<String>(mockUrl)).thenAnswer((_) async => const Right(mockValue));
+  group('MqRemoteClient `get`, `getType`, `getListOfType`', () {
+    test('Get', () async {
+      Map<String, dynamic>? mapValue;
+      List<dynamic>? listValue;
 
-      final result = await client.get<String>(mockUrl);
+      final mapResponse = await client.get<Map<String, dynamic>>(getUrl);
+      mapResponse.fold((l) => null, (r) => mapValue = r);
 
-      final value = result.fold(
-        (error) => fail('Expected a successful result but got an error: $error'),
-        (data) => data,
-      );
+      expect(mapValue, isNotNull);
+      expect(mapValue, isMap);
+      expect(mapValue, isA<Map<String, dynamic>>());
 
-      expect(value, mockValue);
+      final listResponse = await client.get<List<dynamic>>(getListUrl);
+      listResponse.fold((l) => null, (r) => listValue = r);
+      expect(listValue, isNotNull);
+      expect(listValue, isList);
+      expect(listValue?.isNotEmpty, true);
+      expect(listValue, isA<List<dynamic>>());
     });
 
-    test('with type', () async {
-      when(
-        () => client.getType<String>(mockUrl, fromJson: any(named: 'fromJson')),
-      ).thenAnswer((_) async => const Right<String, MqRemoteException>(mockValue));
-
-      final result = await client.getType<String>(mockUrl, fromJson: (json) => json as String);
-
-      final value = result.fold(
-        (error) => fail('Expected a successful result but got an error: $error'),
-        (data) => data,
+    test('Get Type', () async {
+      TestModel? testModel;
+      final response = await client.getType<TestModel>(
+        getUrl,
+        fromJson: TestModel.fromJson,
       );
-
-      expect(value, mockValue);
+      response.fold((l) => null, (r) => testModel = r);
+      expect(testModel, isNotNull);
+      expect(testModel, isA<TestModel>());
     });
 
-    test('with list of type', () async {
-      when(
-        () => client.getListOfType<Map<String, dynamic>>(mockUrl, fromJson: any(named: 'fromJson')),
-      ).thenAnswer(
-        (_) async => const Right([
-          {'key': mockValue},
-        ]),
+    test('Get List Of Type', () async {
+      List<TestModel>? testModelList;
+      final response = await client.getListOfType<TestModel>(
+        getListUrl,
+        fromJson: TestModel.fromJson,
       );
-
-      final result = await client.getListOfType<Map<String, dynamic>>(mockUrl, fromJson: (json) => json);
-
-      expect(
-        result,
-        const Right<List<Map<String, dynamic>>, dynamic>([
-          {'key': mockValue},
-        ]),
-      );
-    });
-  });
-
-  group('POST requests', () {
-    test('successful', () async {
-      when(
-        () => client.post<Map<String, dynamic>>(
-          mockUrl,
-          body: requestBody,
-        ),
-      ).thenAnswer(
-        (_) async => const Right({'key': 'value'}),
-      );
-
-      final result = await client.post<Map<String, dynamic>>(
-        mockUrl,
-        body: requestBody,
-      );
-
-      expect(result, const Right<Map<String, dynamic>, dynamic>({'key': 'value'}));
+      response.fold((l) => null, (r) => testModelList = r);
+      expect(testModelList, isNotNull);
+      expect(testModelList, isList);
+      expect(testModelList, isA<List<TestModel>>());
+      expect(testModelList?[0], isA<TestModel>());
     });
 
-    test('with type', () async {
-      when(
-        () => client.postType<Map<String, dynamic>>(
-          mockUrl,
-          body: requestBody,
-          fromJson: any(named: 'fromJson'),
-        ),
-      ).thenAnswer(
-        (_) async => const Right({'key': 'value'}),
+    test('Get empty List', () async {
+      List<TestModel>? testModelList;
+      final response = await client.getListOfType<TestModel>(
+        emptyList,
+        fromJson: TestModel.fromJson,
       );
-
-      final result = await client.postType<Map<String, dynamic>>(
-        mockUrl,
-        body: requestBody,
-        fromJson: (json) => json,
-      );
-
-      expect(result, const Right<Map<String, dynamic>, dynamic>({'key': 'value'}));
-    });
-
-    test('List of type', () async {
-      when(
-        () => client.postListOfType<Map<String, dynamic>>(
-          mockUrl,
-          fromJson: any(named: 'fromJson'),
-        ),
-      ).thenAnswer(
-        (_) async => const Right([
-          {'key': 'value'},
-        ]),
-      );
-
-      final result = await client.postListOfType<Map<String, dynamic>>(
-        mockUrl,
-        fromJson: (json) => json,
-      );
-
-      expect(
-        result,
-        const Right<List<Map<String, dynamic>>, dynamic>([
-          {'key': 'value'},
-        ]),
-      );
-    });
-  });
-
-  group('PUT requests', () {
-    test('successful', () async {
-      when(
-        () => client.put<Map<String, dynamic>>(
-          mockUrl,
-          body: requestBody,
-        ),
-      ).thenAnswer(
-        (_) async => const Right({'key': 'value'}),
-      );
-
-      final result = await client.put<Map<String, dynamic>>(
-        mockUrl,
-        body: requestBody,
-      );
-
-      expect(result, const Right<Map<String, dynamic>, dynamic>({'key': 'value'}));
-    });
-
-    test('with type', () async {
-      when(
-        () => client.putType<Map<String, dynamic>>(
-          mockUrl,
-          body: requestBody,
-          fromJson: any(named: 'fromJson'),
-        ),
-      ).thenAnswer(
-        (_) async => const Right({'key': 'value'}),
-      );
-
-      final result = await client.putType<Map<String, dynamic>>(
-        mockUrl,
-        body: requestBody,
-        fromJson: (json) => json,
-      );
-
-      expect(result, const Right<Map<String, dynamic>, dynamic>({'key': 'value'}));
-    });
-
-    test('list of type', () async {
-      when(
-        () => client.putListOfType<Map<String, dynamic>>(
-          mockUrl,
-          fromJson: any(named: 'fromJson'),
-        ),
-      ).thenAnswer(
-        (_) async => const Right([
-          {'key': 'value'},
-        ]),
-      );
-
-      final result = await client.putListOfType<Map<String, dynamic>>(mockUrl, fromJson: (json) => json);
-
-      expect(
-        result,
-        const Right<List<Map<String, dynamic>>, dynamic>([
-          {'key': 'value'},
-        ]),
-      );
-    });
-  });
-  group('PATCH requests', () {
-    test('successful', () async {
-      when(
-        () => client.patch<Map<String, dynamic>>(mockUrl, body: requestBody, fromJson: any(named: 'fromJson')),
-      ).thenAnswer((_) async => Right<Map<String, dynamic>, MqRemoteException>(mockResponse));
-
-      final result = await client.patch<Map<String, dynamic>>(mockUrl, body: requestBody, fromJson: (json) => json);
-
-      expect(result, isA<Right<Map<String, dynamic>, MqRemoteException>>());
-    });
-
-    test('with type', () async {
-      when(
-        () => client.patchType<Map<String, dynamic>>(mockUrl, body: requestBody, fromJson: any(named: 'fromJson')),
-      ).thenAnswer(
-        (_) async => Right<Map<String, dynamic>, MqRemoteException>(mockResponse),
-      );
-
-      final result = await client.patchType<Map<String, dynamic>>(mockUrl, body: requestBody, fromJson: (json) => json);
-
-      expect(result, isA<Right<Map<String, dynamic>, MqRemoteException>>());
-    });
-
-    test('list of type', () async {
-      when(
-        () => client.patchListOfType<Map<String, dynamic>>(mockUrl, fromJson: any(named: 'fromJson')),
-      ).thenAnswer(
-        (_) async => const Right<List<Map<String, dynamic>>, MqRemoteException>([
-          {'key': 'value'},
-        ]),
-      );
-
-      final result = await client.patchListOfType<Map<String, dynamic>>(mockUrl, fromJson: (json) => json);
-
-      expect(result, isA<Right<List<Map<String, dynamic>>, MqRemoteException>>());
+      response.fold((l) => null, (r) => testModelList = r);
+      expect(testModelList, isNotNull);
+      expect(testModelList, isList);
+      expect(testModelList, isA<List<TestModel>>());
+      expect(testModelList?.isEmpty, true);
     });
   });
 }
