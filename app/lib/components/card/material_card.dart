4f4d6bb308cc8ccf29edb4e45015efaf9bb79051
aidaiym <omurbekovnaaidaiym@gmail.com>
2024-10-31 15:49:53 +0600

    change material card

--- app/lib/components/card/material_card.dart
+++ app/lib/components/card/material_card.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 
 class MaterialCard extends StatelessWidget {
   const MaterialCard({
+    required this.color,
     required this.text,
     required this.textColor,
-    required this.color,
     this.check,
     super.key,
   });
-
+  final Color color;
   final String text;
   final Widget? check;
   final Color textColor;
-  final Color color;
 
   @override
   Widget build(BuildContext context) {
