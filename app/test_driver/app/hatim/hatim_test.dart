006aa0a4cba6d59f2225c5c34a971b38432d5413
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-11-02 00:50:53 +0600

    Added prints

--- app/test_driver/app/hatim/hatim_test.dart
+++ app/test_driver/app/hatim/hatim_test.dart
@@ -4,19 +4,29 @@ import 'package:mq_ci_keys/mq_ci_keys.dart';
 import '../../extensions/extensions.dart';
 
 Future<void> checkHatimJuzs(FlutterDriver driver) async {
+  print('checkHatimJuzs init');
   await driver.waitFor(find.byValueKey(MqKeys.hatimPage));
+  print('checkHatimJuzs waitFor hatimPage');
   await driver.waitFor(find.byValueKey(MqKeys.hatimJuzsList));
+  print('checkHatimJuzs waitFor hatimJuzsList');
   await driver.takeScreenshot(Screenshots.hatimJuzs);
+  print('checkHatimJuzs takeScreenshot');
 }
 
 Future<void> tapHatimJuz(FlutterDriver driver) async {
+  print('tapHatimJuz init');
   await driver.tap(find.byValueKey(MqKeys.hatimJuzIndex(1)));
+  print('tapHatimJuz tap');
   await driver.waitFor(find.byValueKey(MqKeys.hatimSelectPage));
+  print('tapHatimJuz waitFor');
 }
 
 Future<void> checkHatimSelectPage(FlutterDriver driver) async {
+  print('checkHatimSelectPage init');
   await driver.waitFor(find.byValueKey(MqKeys.hatimSelectPage));
+  print('checkHatimSelectPage tap');
   await driver.takeScreenshot(Screenshots.hatimPages);
+  print('checkHatimSelectPage takeScreenshot');
 }
 
 // Future<void> backHatimJuzs(FlutterDriver driver) async {
