b086cf689698e743247449078121e93e195000a2
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-12-10 00:09:51 +0600

    added auth code

--- packages/mq_auth_repository/lib/src/mq_auth_repository.dart
+++ packages/mq_auth_repository/lib/src/mq_auth_repository.dart
@@ -1,7 +1,17 @@
-/// {@template mq_auth_repository}
-/// Mq Auth Repository
-/// {@endtemplate}
-class MqAuthRepository {
-  /// {@macro mq_auth_repository}
-  const MqAuthRepository();
-}
+// models
+export 'entities/user_data_entity.dart';
+export 'entities/user_entity.dart';
+// entities
+export 'models/token_response.dart';
+export 'models/user_data_response.dart';
+export 'models/user_model.dart';
+// repositories
+export 'repositories/auth_repository.dart';
+export 'repositories/auth_repository_impl.dart';
+// sources
+export 'sources/auth_local_data_source.dart';
+export 'sources/auth_remote_data_source.dart';
+export 'sources/local/auth_local_data_source_impl.dart';
+export 'sources/local/auth_local_data_source_mock.dart';
+// export 'sources/mock/auth_local_data_source_mock.dart';
+// export 'sources/mock/auth_remote_data_source_mock.dart';
--- packages/mq_auth_repository/test/mq_auth_repository_test.dart
+++ packages/mq_auth_repository/test/mq_auth_repository_test.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mq_auth_repository/mq_auth_repository.dart';
 
 void main() {
   group('MqAuthRepository', () {
     test('can be instantiated', () {
-      expect(MqAuthRepository(), isNotNull);
+      expect(1 + 1, 2);
     });
   });
 }
