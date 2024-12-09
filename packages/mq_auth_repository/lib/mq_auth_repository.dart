f6ea7870392d0646d76b4bf773d8715ac57e199e
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-12-09 23:26:10 +0600

    Created mq_auth_repository pkg

---
+++ packages/mq_auth_repository/lib/mq_auth_repository.dart
@@ -0,0 +1,4 @@
+/// Mq Auth Repository
+library;
+
+export 'src/mq_auth_repository.dart';
---
+++ packages/mq_auth_repository/lib/src/mq_auth_repository.dart
@@ -0,0 +1,7 @@
+/// {@template mq_auth_repository}
+/// Mq Auth Repository
+/// {@endtemplate}
+class MqAuthRepository {
+  /// {@macro mq_auth_repository}
+  const MqAuthRepository();
+}
