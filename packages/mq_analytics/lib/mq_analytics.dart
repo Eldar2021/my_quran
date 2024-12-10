85924709a7014416541eb357cf409e81454f33f4
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-12-10 23:54:42 +0600

    Added mq_analytics

---
+++ packages/mq_analytics/lib/mq_analytics.dart
@@ -0,0 +1,4 @@
+/// MyQuran Analytics package
+library;
+
+export 'src/mq_analytics.dart';
---
+++ packages/mq_analytics/lib/src/mq_analytics.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
+
+@immutable
+final class MqAnalytic {
+  const MqAnalytic._();
+
+  static Future<void> setUserProperty(String userName) async {
+    try {
+      if (kDebugMode) return;
+      await _analytics.setUserProperty(name: userName, value: null);
+    } catch (error, stackTrace) {
+      MqCrashlytics.report(error, stackTrace);
+      log('MqAnalytics setUserProperty error: $error, stackTrace: $stackTrace');
+    }
+  }
+
+  static void track(String event, {Map<String, Object>? params}) {
+    try {
+      if (kDebugMode) return;
+      _analytics.logEvent(name: event, parameters: params);
+    } catch (error, stackTrace) {
+      MqCrashlytics.report(error, stackTrace);
+      log('MqAnalytics track error: $error, stackTrace: $stackTrace');
+    }
+  }
+
+  static FirebaseAnalytics get _analytics => FirebaseAnalytics.instance;
+}
