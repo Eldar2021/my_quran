b2d9b630f28a13d32560390fc4176feb3db08812
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-12-19 22:41:44 +0600

    formatted

--- packages/mq_quran_repository/lib/src/model/filters_response.g.dart
+++ packages/mq_quran_repository/lib/src/model/filters_response.g.dart
@@ -6,12 +6,10 @@ part of 'filters_response.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-FiltersResponse _$FiltersResponseFromJson(Map<String, dynamic> json) =>
-    FiltersResponse(
+FiltersResponse _$FiltersResponseFromJson(Map<String, dynamic> json) => FiltersResponse(
       pageNumber: json['page_number'] as String,
     );
 
-Map<String, dynamic> _$FiltersResponseToJson(FiltersResponse instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$FiltersResponseToJson(FiltersResponse instance) => <String, dynamic>{
       'page_number': instance.pageNumber,
     };
--- packages/mq_quran_repository/lib/src/model/verse_response.g.dart
+++ packages/mq_quran_repository/lib/src/model/verse_response.g.dart
@@ -6,15 +6,13 @@ part of 'verse_response.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-VerseResponse _$VerseResponseFromJson(Map<String, dynamic> json) =>
-    VerseResponse(
+VerseResponse _$VerseResponseFromJson(Map<String, dynamic> json) => VerseResponse(
       id: (json['id'] as num).toInt(),
       verseKey: json['verse_key'] as String,
       textUthmani: json['text_imlaei'] as String,
     );
 
-Map<String, dynamic> _$VerseResponseToJson(VerseResponse instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$VerseResponseToJson(VerseResponse instance) => <String, dynamic>{
       'id': instance.id,
       'verse_key': instance.verseKey,
       'text_imlaei': instance.textUthmani,
