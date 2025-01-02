19f531f801fe3fabab60766accac2b40e176bc4c
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-02 11:06:49 +0600

    Added colors and typogryphy

---
+++ packages/mq_app_ui/lib/src/colors/app_colors.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  /// colorsBrOrange figma style properties
+  /// hex: #d95a38
+  /// opacity: 1
+  static const Color colorsBrOrange = Color(0xffd95a38);
+
+  /// colorsBrGradient figma style properties
+  /// linear gradient
+  /// start,stop hex: (#df7643,#d74931)
+  /// start,stop opacity: (1,1)
+  static const List<Color> colorsBrGradient = <Color>[
+    Color(0xffdf7643),
+    Color(0xffd74931),
+  ];
+
+  /// colorsBrBlue figma style properties
+  /// hex: #235190
+  /// opacity: 1
+  static const Color colorsBrBlue = Color(0xff235190);
+
+  /// colorsBrGradient2 figma style properties
+  /// linear gradient
+  /// start,stop hex: (#97c6fa,#408af0)
+  /// start,stop opacity: (1,1)
+  static const List<Color> colorsBrGradient2 = <Color>[
+    Color(0xff97c6fa),
+    Color(0xff408af0),
+  ];
+
+  /// colorsBrBlue2 figma style properties
+  /// hex: #68abf8
+  /// opacity: 1
+  static const Color colorsBrBlue2 = Color(0xff68abf8);
+
+  /// colorsBrBlue3 figma style properties
+  /// hex: #408af0
+  /// opacity: 1
+  static const Color colorsBrBlue3 = Color(0xff408af0);
+
+  /// colorsBrBlue4 figma style properties
+  /// hex: #f1f4fd
+  /// opacity: 1
+  static const Color colorsBrBlue4 = Color(0xfff1f4fd);
+
+  /// colorsBrShadow figma style properties
+  /// hex: #373737
+  /// opacity: 1
+  static const Color colorsBrShadow = Color(0xff373737);
+
+  /// colorsBrBlackBg figma style properties
+  /// hex: #0e0e0f
+  /// opacity: 1
+  static const Color colorsBrBlackBg = Color(0xff0e0e0f);
+
+  /// colorsBrDarkmode figma style properties
+  /// hex: #1c1c1e
+  /// opacity: 1
+  static const Color colorsBrDarkmode = Color(0xff1c1c1e);
+
+  /// colorsBrBookBg figma style properties
+  /// hex: #fceed9
+  /// opacity: 1
+  static const Color colorsBrBookBg = Color(0xfffceed9);
+
+  /// colorsBrBlack figma style properties
+  /// hex: #2a2b30
+  /// opacity: 1
+  static const Color colorsBrBlack = Color(0xff2a2b30);
+
+  /// colorsBrBlack2 figma style properties
+  /// hex: #6e707f
+  /// opacity: 1
+  static const Color colorsBrBlack2 = Color(0xff6e707f);
+
+  /// colorsBrBlack3 figma style properties
+  /// hex: #1d1d1d
+  /// opacity: 1
+  static const Color colorsBrBlack3 = Color(0xff1d1d1d);
+
+  /// colorsBrBlack4 figma style properties
+  /// hex: #222222
+  /// opacity: 1
+  static const Color colorsBrBlack4 = Color(0xff222222);
+
+  /// colorsDarkBottomNav figma style properties
+  /// hex: #222222
+  /// opacity: 1
+  static const Color colorsDarkBottomNav = Color(0xff222222);
+
+  /// colorsDarkCardBg figma style properties
+  /// hex: #353333
+  /// opacity: 1
+  static const Color colorsDarkCardBg = Color(0xff353333);
+
+  /// colorsBrError figma style properties
+  /// hex: #952116
+  /// opacity: 1
+  static const Color colorsBrError = Color(0xff952116);
+
+  /// colorsBrError2 figma style properties
+  /// hex: #eb5244
+  /// opacity: 1
+  static const Color colorsBrError2 = Color(0xffeb5244);
+
+  /// colorsBrGreen figma style properties
+  /// hex: #69b468
+  /// opacity: 1
+  static const Color colorsBrGreen = Color(0xff69b468);
+
+  /// colorsBrYellow figma style properties
+  /// hex: #f4b963
+  /// opacity: 1
+  static const Color colorsBrYellow = Color(0xfff4b963);
+
+  /// colorsBrLinearWhite figma style properties
+  /// hex: #ffffff
+  /// opacity: 1
+  static const Color colorsBrLinearWhite = Color(0xffffffff);
+
+  /// colorsBrWhite figma style properties
+  /// hex: #ffffff
+  /// opacity: 1
+  static const Color colorsBrWhite = Color(0xffffffff);
+
+  /// colorsBrWhite2 figma style properties
+  /// hex: #f3f3f3
+  /// opacity: 1
+  static const Color colorsBrWhite2 = Color(0xfff3f3f3);
+
+  /// colorsBrGrey figma style properties
+  /// hex: #9fa1b3
+  /// opacity: 1
+  static const Color colorsBrGrey = Color(0xff9fa1b3);
+
+  /// colorsBrGrey2 figma style properties
+  /// hex: #e3e3e3
+  /// opacity: 1
+  static const Color colorsBrGrey2 = Color(0xffe3e3e3);
+
+  /// colorsBrGrey3 figma style properties
+  /// hex: #bdc4cd
+  /// opacity: 1
+  static const Color colorsBrGrey3 = Color(0xffbdc4cd);
+
+  /// colorsBrGrey4 figma style properties
+  /// hex: #9f9f9f
+  /// opacity: 1
+  static const Color colorsBrGrey4 = Color(0xff9f9f9f);
+}
