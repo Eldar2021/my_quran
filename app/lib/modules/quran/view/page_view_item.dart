d1f26bbb2cfa8d1a33a64f63ba435b3405c6236a
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-07 03:53:29 +0600

    Finish Quran

--- app/lib/modules/quran/view/page_view_item.dart
+++ app/lib/modules/quran/view/page_view_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mq_analytics/mq_analytics.dart';
+import 'package:mq_app_ui/mq_app_ui.dart';
 import 'package:mq_ci_keys/mq_ci_keys.dart';
 import 'package:mq_quran_repository/mq_quran_repository.dart';
 
@@ -14,66 +15,70 @@ class PageViewItem<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 20),
+    final colorScheme = Theme.of(context).colorScheme;
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 24),
       itemCount: items.length,
       itemBuilder: (BuildContext context, int index) {
         final item = items[index];
         if (item is JuzEntity) {
-          return Card(
-            child: ListTile(
-              key: Key(MqKeys.quranReadJus(index)),
-              minLeadingWidth: 10,
-              leading: Text('${item.id}'),
-              title: Text('${item.id}-${context.l10n.juz}'),
-              subtitle: Text(item.name),
-              onTap: () async {
-                MqAnalytic.track(AnalyticKey.goQuranReadByJuz, params: {'juzId': item.id});
-                final pages = <int>[];
-                for (var i = item.pages.first; i <= item.pages.last; i++) {
-                  pages.add(i);
-                }
-                pages.sort();
-                context.goNamed(
-                  AppRouter.read,
-                  pathParameters: {
-                    'pages': pages.toString(),
-                    'isHatim': true.toString(),
-                  },
-                );
-              },
-            ),
+          return QuranItemTile(
+            key: Key(MqKeys.quranReadJus(index)),
+            index: item.id,
+            title: '${item.id}-${context.l10n.juz}',
+            subtitle: item.name,
+            onTap: () async {
+              MqAnalytic.track(
+                AnalyticKey.goQuranReadByJuz,
+                params: {'juzId': item.id},
+              );
+              final pages = <int>[];
+              for (var i = item.pages.first; i <= item.pages.last; i++) {
+                pages.add(i);
+              }
+              pages.sort();
+              context.goNamed(
+                AppRouter.read,
+                pathParameters: {
+                  'pages': pages.toString(),
+                  'isHatim': true.toString(),
+                },
+              );
+            },
           );
         } else if (item is SurahEntity) {
-          return Card(
-            child: ListTile(
-              key: Key(MqKeys.quranReadSurah(index)),
-              minLeadingWidth: 10,
-              leading: Text('${item.id}'),
-              title: Text(item.arabic),
-              subtitle: Text(item.name),
-              onTap: () async {
-                MqAnalytic.track(
-                  AnalyticKey.goQuranReadBySurah,
-                  params: {'surahId': item.id, 'surahName': item.name},
-                );
-                final pages = <int>[];
-                for (var i = item.pages.first; i <= item.pages.last; i++) {
-                  pages.add(i);
-                }
-                pages.sort();
-                context.goNamed(
-                  AppRouter.read,
-                  pathParameters: {
-                    'pages': pages.toString(),
-                    'isHatim': true.toString(),
-                  },
-                );
-              },
-            ),
+          return QuranItemTile(
+            key: Key(MqKeys.quranReadSurah(index)),
+            index: item.id,
+            title: item.arabic,
+            subtitle: item.name,
+            onTap: () async {
+              MqAnalytic.track(
+                AnalyticKey.goQuranReadBySurah,
+                params: {'surahId': item.id, 'surahName': item.name},
+              );
+              final pages = <int>[];
+              for (var i = item.pages.first; i <= item.pages.last; i++) {
+                pages.add(i);
+              }
+              pages.sort();
+              context.goNamed(
+                AppRouter.read,
+                pathParameters: {
+                  'pages': pages.toString(),
+                  'isHatim': true.toString(),
+                },
+              );
+            },
           );
         }
         return const SizedBox();
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(
+          height: 0.5,
+          color: colorScheme.onSurface.withOpacity(0.1),
+        );
       },
     );
   }
--- app/lib/modules/quran/view/quran_view.dart
+++ app/lib/modules/quran/view/quran_view.dart
@@ -5,7 +5,6 @@ import 'package:mq_ci_keys/mq_ci_keys.dart';
 import 'package:mq_quran_repository/mq_quran_repository.dart';
 import 'package:mq_remote_client/mq_remote_client.dart';
 import 'package:mq_storage/mq_storage.dart';
-
 import 'package:my_quran/components/components.dart';
 import 'package:my_quran/l10n/l10.dart';
 import 'package:my_quran/modules/modules.dart';
@@ -42,16 +41,28 @@ class QuranBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
         key: const Key(MqKeys.quaranReadInitPage),
         title: Text(context.l10n.quran),
-        bottom: TabBar(
-          indicatorColor: context.colors.primary,
-          labelColor: context.colors.onSurfaceVariant,
+        bottom: TabBar.secondary(
+          labelColor: context.colors.primary,
+          indicatorPadding: const EdgeInsets.symmetric(horizontal: 24),
+          labelStyle: context.textTheme.titleMedium?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+          dividerColor: colorScheme.surfaceBright,
+          dividerHeight: 3,
           tabs: [
-            PageViewCard(page: context.l10n.juzs, key: const Key(MqKeys.quaranReadJuzs)),
-            PageViewCard(page: context.l10n.surahs, key: const Key(MqKeys.quaranReadSurahs)),
+            PageViewCard(
+              page: context.l10n.juzs,
+              key: const Key(MqKeys.quaranReadJuzs),
+            ),
+            PageViewCard(
+              page: context.l10n.surahs,
+              key: const Key(MqKeys.quaranReadSurahs),
+            ),
           ],
         ),
       ),
