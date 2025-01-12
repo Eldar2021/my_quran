cd9bc32449dfa6ca0d09908c289aee406f4da814
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-13 02:03:13 +0600

    Added MyQuran Stories cubit

---
+++ app/lib/modules/home/cubit/mq_story_state.dart
@@ -0,0 +1,15 @@
+part of 'mq_story_cubit.dart';
+
+@immutable
+final class MqStoryState extends Equatable {
+  const MqStoryState({
+    this.status = FetchStatus.loading,
+    this.stories,
+  });
+
+  final List<MqStoryEntity>? stories;
+  final FetchStatus status;
+
+  @override
+  List<Object?> get props => [stories, status];
+}
--- app/lib/modules/home/home.dart
+++ app/lib/modules/home/home.dart
@@ -1,5 +1,6 @@
 export 'cubit/home_cubit.dart';
 export 'cubit/location_cubit.dart';
+export 'cubit/mq_story_cubit.dart';
 export 'view/home_view.dart';
 export 'widgets/pray_time_widget.dart';
 export 'widgets/home_drawer.dart';
