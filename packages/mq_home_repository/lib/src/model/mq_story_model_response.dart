8be218bc538cd4966d54e83059a1991740d8ef67
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-13 01:38:38 +0600

    Added MqStoryModelEntity

---
+++ packages/mq_home_repository/lib/src/model/mq_story_model_response.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:mq_home_repository/mq_home_repository.dart';
+
+part 'mq_story_model_response.g.dart';
+
+@JsonSerializable()
+@immutable
+final class MqStoryModelResponse {
+  const MqStoryModelResponse({
+    required this.cardImageUrl,
+    required this.cardLabel,
+    required this.isViewed,
+    required this.button,
+    required this.screens,
+  });
+
+  factory MqStoryModelResponse.fromJson(Map<String, dynamic> json) => _$MqStoryModelResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MqStoryModelResponseToJson(this);
+
+  final String cardImageUrl;
+  final String cardLabel;
+  final bool isViewed;
+  final MqStoryButtonResponse button;
+  final List<MqStoryScreenResponse> screens;
+
+  MqStoryModelEntity toEntity() {
+    return MqStoryModelEntity(
+      cardImageUrl: cardImageUrl,
+      cardLabel: cardLabel,
+      isViewed: isViewed,
+      button: button.toEntity(),
+      screens: screens.map((e) => e.toEntity()).toList(),
+    );
+  }
+}
+
+@JsonSerializable()
+@immutable
+final class MqStoryButtonResponse {
+  const MqStoryButtonResponse({
+    required this.hasButton,
+    required this.label,
+    required this.action,
+  });
+
+  factory MqStoryButtonResponse.fromJson(Map<String, dynamic> json) => _$MqStoryButtonResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MqStoryButtonResponseToJson(this);
+
+  final bool hasButton;
+  final String label;
+  final String action;
+
+  MqStoryButtonEntity toEntity() {
+    return MqStoryButtonEntity(
+      hasButton: hasButton,
+      label: label,
+      action: action,
+    );
+  }
+}
+
+@JsonSerializable()
+@immutable
+final class MqStoryScreenResponse {
+  const MqStoryScreenResponse({
+    required this.imageUrl,
+    required this.durationByMilliseconds,
+  });
+
+  factory MqStoryScreenResponse.fromJson(Map<String, dynamic> json) => _$MqStoryScreenResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MqStoryScreenResponseToJson(this);
+
+  final String imageUrl;
+  final int durationByMilliseconds;
+
+  MqStoryScreenEntity toEntity() {
+    return MqStoryScreenEntity(
+      imageUrl: imageUrl,
+      durationByMilliseconds: durationByMilliseconds,
+    );
+  }
+}
---
+++ packages/mq_home_repository/lib/src/model/mq_story_model_response.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mq_story_model_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MqStoryModelResponse _$MqStoryModelResponseFromJson(Map<String, dynamic> json) => MqStoryModelResponse(
+      cardImageUrl: json['cardImageUrl'] as String,
+      cardLabel: json['cardLabel'] as String,
+      isViewed: json['isViewed'] as bool,
+      button: MqStoryButtonResponse.fromJson(json['button'] as Map<String, dynamic>),
+      screens: (json['screens'] as List<dynamic>)
+          .map((e) => MqStoryScreenResponse.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$MqStoryModelResponseToJson(MqStoryModelResponse instance) => <String, dynamic>{
+      'cardImageUrl': instance.cardImageUrl,
+      'cardLabel': instance.cardLabel,
+      'isViewed': instance.isViewed,
+      'button': instance.button,
+      'screens': instance.screens,
+    };
+
+MqStoryButtonResponse _$MqStoryButtonResponseFromJson(Map<String, dynamic> json) => MqStoryButtonResponse(
+      hasButton: json['hasButton'] as bool,
+      label: json['label'] as String,
+      action: json['action'] as String,
+    );
+
+Map<String, dynamic> _$MqStoryButtonResponseToJson(MqStoryButtonResponse instance) => <String, dynamic>{
+      'hasButton': instance.hasButton,
+      'label': instance.label,
+      'action': instance.action,
+    };
+
+MqStoryScreenResponse _$MqStoryScreenResponseFromJson(Map<String, dynamic> json) => MqStoryScreenResponse(
+      imageUrl: json['imageUrl'] as String,
+      durationByMilliseconds: (json['durationByMilliseconds'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$MqStoryScreenResponseToJson(MqStoryScreenResponse instance) => <String, dynamic>{
+      'imageUrl': instance.imageUrl,
+      'durationByMilliseconds': instance.durationByMilliseconds,
+    };
