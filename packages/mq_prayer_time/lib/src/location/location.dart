d260b4f587ae13a1390a63fc5cab14c2fe11b34c
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-01-15 22:25:10 +0600

    Refactored

---
+++ packages/mq_prayer_time/lib/src/location/location.dart
@@ -0,0 +1,5 @@
+export 'service/mq_location_service.dart';
+export 'service/mq_location_service_impl.dart';
+export 'storage/mq_location_storage.dart';
+export 'storage/mq_location_storage_impl.dart';
+export 'storage/mq_location_storage_mock.dart';
--- packages/mq_prayer_time/lib/src/mq_prayer_time.dart
+++ packages/mq_prayer_time/lib/src/mq_prayer_time.dart
@@ -1,5 +1,1 @@
-export 'location/mq_location_service.dart';
-export 'location/mq_location_service_impl.dart';
-export 'storage/mq_location_storage.dart';
-export 'storage/mq_location_storage_impl.dart';
-export 'storage/mq_location_storage_mock.dart';
+export 'location/location.dart';
