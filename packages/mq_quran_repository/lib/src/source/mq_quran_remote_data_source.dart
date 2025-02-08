10771ca320778b9f83db7a3e631c1424210c9923
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-02-09 01:24:40 +0600

    vekcdf

--- packages/mq_quran_repository/lib/src/source/mq_quran_remote_data_source.dart
+++ packages/mq_quran_repository/lib/src/source/mq_quran_remote_data_source.dart
@@ -2,4 +2,19 @@ import 'package:mq_quran_repository/mq_quran_repository.dart';
 
 abstract class MqQuranRemoteDataSource {
   Future<QuranPageResponse?> fetchPage(int page, String quranFmt);
+
+  Future<QuranDataResponse> fetchQuranByPage(
+    int pageNumber,
+    String quranFmt,
+  );
+
+  Future<QuranDataResponse> fetchQuranByJuz(
+    int juzNumber,
+    String quranFmt,
+  );
+
+  Future<QuranDataResponse> fetchQuranBySurah(
+    int surahNumber,
+    String quranFmt,
+  );
 }
--- packages/mq_quran_repository/lib/src/source/remote/mq_quran_remote_data_source_impl.dart
+++ packages/mq_quran_repository/lib/src/source/remote/mq_quran_remote_data_source_impl.dart
@@ -20,4 +20,52 @@ final class MqQuranRemoteDataSourceImpl implements MqQuranRemoteDataSource {
       (r) => r,
     );
   }
+
+  @override
+  Future<QuranDataResponse> fetchQuranByJuz(
+    int juzNumber,
+    String quranFmt,
+  ) async {
+    final remoteValue = await remoteClient.getType<QuranDataResponse>(
+      'https://api.quran.com/api/v4/quran/verses/$quranFmt?juz_number=$juzNumber',
+      fromJson: QuranDataResponse.fromJson,
+    );
+
+    return remoteValue.fold(
+      (l) => throw Exception('Failed to fetch remote data $l'),
+      (r) => r,
+    );
+  }
+
+  @override
+  Future<QuranDataResponse> fetchQuranByPage(
+    int pageNumber,
+    String quranFmt,
+  ) async {
+    final remoteValue = await remoteClient.getType<QuranDataResponse>(
+      'https://api.quran.com/api/v4/quran/verses/$quranFmt?page_number=$pageNumber',
+      fromJson: QuranDataResponse.fromJson,
+    );
+
+    return remoteValue.fold(
+      (l) => throw Exception('Failed to fetch remote data $l'),
+      (r) => r,
+    );
+  }
+
+  @override
+  Future<QuranDataResponse> fetchQuranBySurah(
+    int surahNumber,
+    String quranFmt,
+  ) async {
+    final remoteValue = await remoteClient.getType<QuranDataResponse>(
+      'https://api.quran.com/api/v4/quran/verses/$quranFmt?chapter_number=$surahNumber',
+      fromJson: QuranDataResponse.fromJson,
+    );
+
+    return remoteValue.fold(
+      (l) => throw Exception('Failed to fetch remote data $l'),
+      (r) => r,
+    );
+  }
 }
