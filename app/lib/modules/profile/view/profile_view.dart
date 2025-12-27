3cf6d71dd47af33e8ee18c2de33642ba51a40686
Eldar2021 <eldiiaralmazbekov@gmail.com>
2025-12-28 02:40:25 +0600

    docs: Add profile data structure example comment to profile view.

--- app/lib/modules/profile/view/profile_view.dart
+++ app/lib/modules/profile/view/profile_view.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+/*
+{
+  "first_name": "Eldiiar",
+  "last_name": "Almazbek",
+  "username": "eldiiar",
+  "avatar": "https://lh3.",
+  "email": "eldiiaralmazbekov@gmail.com",
+  "country": null,
+  "phone_number": null,
+  "gender": "male",
+  "language": "ky",
+  "can_create_hatim": true, // Done
+  "allow_notifications": true, // Done
+  "timezone": "Asia/Bishkek"
+}
+*/
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
