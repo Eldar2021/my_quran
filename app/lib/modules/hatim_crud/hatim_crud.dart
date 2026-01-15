700b604d5a4e750e9c916da2c696e9f5521db148
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-01-15 22:40:49 +0600

    feat: Implement Hatim CRUD view with form fields, custom widgets, and repository integration.

--- app/lib/modules/hatim_crud/hatim_crud.dart
+++ app/lib/modules/hatim_crud/hatim_crud.dart
@@ -3,3 +3,5 @@ export 'cubit/hatim_crud_participants_cubit.dart';
 export 'view/hatim_crud_view.dart';
 export 'view/hatim_participants_view.dart';
 export 'view/hatim_crud_succes_view.dart';
+export 'widgets/form_label.dart';
+export 'widgets/hatim_crud_text_field.dart';
--- app/lib/modules/hatim_crud/view/hatim_crud_view.dart
+++ app/lib/modules/hatim_crud/view/hatim_crud_view.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mq_app_ui/mq_app_ui.dart';
+import 'package:mq_ci_keys/mq_ci_keys.dart';
+import 'package:mq_hatim_repository/mq_hatim_repository.dart';
+import 'package:my_quran/l10n/l10.dart';
+import 'package:my_quran/modules/modules.dart';
 
 class HatimCrudView extends StatelessWidget {
   const HatimCrudView(this.hatimId, {super.key});
@@ -7,9 +13,112 @@ class HatimCrudView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => HatimCrudBloc(
+        repository: context.read<MqHatimRepository>(),
+        hatimId: hatimId,
+      ),
+      child: const _Body(),
+    );
+  }
+}
+
+class _Body extends StatefulWidget {
+  const _Body();
+
+  @override
+  State<_Body> createState() => __BodyState();
+}
+
+class __BodyState extends State<_Body> {
+  late final GlobalKey<FormState> _formKey;
+  late final TextEditingController _titleCtl;
+  late final TextEditingController _descriptionCtl;
+  late final ValueNotifier<List<MqUserIdModel>> _participants;
+
+  @override
+  void initState() {
+    _formKey = GlobalKey<FormState>();
+    _titleCtl = TextEditingController();
+    _descriptionCtl = TextEditingController();
+    _participants = ValueNotifier([]);
+    super.initState();
+    final bloc = context.read<HatimCrudBloc>();
+    if (bloc.hatimId != null) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        bloc.add(GetHatimDataByIdEvent(bloc.hatimId!));
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isUpdate = context.read<HatimCrudBloc>().hatimId != null;
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
+      key: const Key(MqKeys.createHatim),
       appBar: AppBar(
-        title: Text('Hatim Crud $hatimId'),
+        centerTitle: true,
+        title: Text(isUpdate ? 'Update Hatim' : context.l10n.createHatim),
+      ),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          children: [
+            const SizedBox(height: 24),
+            FormLabel(context.l10n.title),
+            HatimCrudTextField(
+              key: const Key(MqKeys.titleTextField),
+              controller: _titleCtl,
+              validator: (v) => (v == null || v.isEmpty) ? context.l10n.enterHatimTitle : null,
+              hintText: context.l10n.enterHatimTitleHere,
+              prefixIcon: Assets.icons.title.svg(
+                colorFilter: ColorFilter.mode(
+                  colorScheme.primary,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+            const SizedBox(height: 26),
+            FormLabel(context.l10n.description),
+            HatimCrudTextField(
+              key: const Key(MqKeys.descriptionTextField),
+              controller: _descriptionCtl,
+              maxLines: 5,
+              validator: (v) => (v == null || v.isEmpty) ? context.l10n.enterHatimDescription : null,
+              hintText: context.l10n.enterHatimDescriptionHere,
+              prefixIcon: Assets.icons.fluentDescription.svg(
+                colorFilter: ColorFilter.mode(
+                  colorScheme.primary,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+            const SizedBox(height: 26),
+            FormLabel(context.l10n.participants),
+            ExcludeFocus(
+              child: HatimCrudTextField(
+                onTap: () {},
+                validator: (_) => _participants.value.isEmpty ? context.l10n.addParticipantsToHatim : null,
+                readOnly: true,
+                hintText: context.l10n.addParticipantsToYourHatim,
+                prefixIcon: Assets.icons.solidUsers.svg(
+                  colorFilter: ColorFilter.mode(
+                    colorScheme.primary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                suffixIcon: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: colorScheme.inverseSurface,
+                  size: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
