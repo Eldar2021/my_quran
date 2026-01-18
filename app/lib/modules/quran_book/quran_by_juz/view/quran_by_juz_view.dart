8d11195b46480f967200419c29c7b0907dc6f498
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-01-18 15:39:26 +0600

    refactor: update text styles to use `headlineSmall` across Quran display widgets.

--- app/lib/modules/quran_book/quran_by_juz/view/quran_by_juz_view.dart
+++ app/lib/modules/quran_book/quran_by_juz/view/quran_by_juz_view.dart
@@ -61,7 +61,7 @@ class __QuranByJuzViewState extends State<_QuranByJuzView> {
               MqQuranStatic.bismallah,
               maxLines: 2,
               textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
                 fontFamily: FontFamily.uthmanicV2,
                 color: themeCubit.state.frColor,
               ),
