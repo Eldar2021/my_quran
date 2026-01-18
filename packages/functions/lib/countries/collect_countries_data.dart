deb3c567d235b7cad90e837f08220d7cc85e7d9d
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-01-18 11:56:15 +0600

    feat: add static country data to `mq_auth_repository` package."

--- packages/functions/lib/countries/collect_countries_data.dart
+++ packages/functions/lib/countries/collect_countries_data.dart
@@ -15,60 +15,18 @@ void main(List<String> args) async {
       .map(
         (e) => {
           'code': e.code,
-          'codeNumeric': e.codeNumeric,
           'codeShort': e.codeShort,
-          'cioc': e.cioc,
           'emoji': e.emoji,
-          'continent': e.continent.name,
-          'subregion': e.subregion?.name,
-          'lat': e.latLng.latitude,
-          'long': e.latLng.longitude,
-          'areaMetric': e.areaMetric,
-          'name': {
-            'officialEn': e.name.official,
-            'commonEn': e.name.common,
-            'nameNative': e.namesNative.first.official,
-            'commonNative': e.namesNative.first.common,
-            'altSpellings': e.altSpellings,
-          },
-          'languages': e.languages
-              .map(
-                (l) => {
-                  'name': l.name,
-                  'code': l.code,
-                  'codeShort': l.codeShort,
-                  'namesNative': l.namesNative,
-                },
-              )
-              .toList(),
-          'tld': e.tld,
+          'officialEn': e.name.official,
+          'commonEn': e.name.common,
+          'nameNative': e.namesNative.first.official,
+          'commonNative': e.namesNative.first.common,
+          'altSpellings': e.altSpellings,
+          'timezones': e.timezones,
           'idd': {
             'root': e.idd.root,
             'suffixes': e.idd.suffixes,
           },
-          'maps': {
-            'googleMaps': e.maps.googleMaps,
-            'openStreetMaps': e.maps.openStreetMaps,
-          },
-          'population': e.population,
-          'fifa': e.fifa,
-          'timezones': e.timezones,
-          'postalCode': {
-            'format': e.postalCode?.format,
-            'regex': e.postalCode?.regExpPattern,
-          },
-          'currency': e.currencies
-              ?.map(
-                (c) => {
-                  'code': c.code,
-                  'name': c.name,
-                  'symbol': c.symbol,
-                  'alternateSymbols': c.alternateSymbols,
-                  'namesNative': c.namesNative,
-                  'subunit': c.subunit,
-                },
-              )
-              .toList(),
         },
       )
       .toList();
