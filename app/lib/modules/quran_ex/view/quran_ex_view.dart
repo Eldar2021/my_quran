13384c3339f118a5425890b7fcca6509ed433513
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-02-13 23:07:15 +0600

    Added new fonts

--- app/lib/modules/quran_ex/view/quran_ex_view.dart
+++ app/lib/modules/quran_ex/view/quran_ex_view.dart
@@ -34,58 +34,105 @@ class _QuranExViewState extends State<QuranExView> {
         backgroundColor: const Color(0xffFFEED6),
         title: const Text('Quran Ex'),
       ),
-      body: PageView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) {
-          return SingleChildScrollView(
-            padding: const EdgeInsets.all(20),
-            child: Center(
-              child: Directionality(
-                textDirection: TextDirection.rtl,
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: _quranExModel.verses.map((verse) {
-                      var codeV2 = verse.codeV2;
-                      if (verse.id == 1) {
-                        codeV2 = '${verse.codeV2}\n';
-                      } else if (verse.id == 2) {
-                        codeV2 = '${verse.codeV2}\n';
-                      } else if (verse.id == 4) {
-                        codeV2 = '${verse.codeV2}\n';
-                      } else if (verse.id == 6) {
-                        final char = verse.codeV2.characters.first;
-                        final char2 = verse.codeV2.split(char).last;
-                        codeV2 = '$char\n$char2';
-                      } else if (verse.id == 7) {
-                        final char = verse.codeV2.characters.toList();
-                        final char1 = verse.codeV2.split(char[6]).first;
-                        final char2 = verse.codeV2.split(char[4]).last;
-                        final char3 = char2.split(char[14]).first;
-                        final char4 = char2.split(char[12]).last;
-                        codeV2 = '$char1\n$char3\n$char4';
-                      }
-                      return TextSpan(
-                        text: codeV2,
-                        locale: const Locale('ar'),
-                        style: const TextStyle(
-                          // fontFamily: FontFamily.page1V2,
-                          fontFamily: FontFamily.page1Woff2,
-                          package: 'mq_app_ui',
-                          fontSize: 28,
-                          height: 1.8,
-                          color: Colors.black,
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: _quranExModel.verses.map((verse) {
+                  var codeV2 = verse.codeV2;
+                  if (verse.id == 1) {
+                    codeV2 = '${verse.codeV2}\n';
+                  } else if (verse.id == 2) {
+                    codeV2 = '${verse.codeV2}\n';
+                  } else if (verse.id == 4) {
+                    codeV2 = '${verse.codeV2}\n';
+                  } else if (verse.id == 6) {
+                    final char = verse.codeV2.characters.first;
+                    final char2 = verse.codeV2.split(char).last;
+                    codeV2 = '$char\n$char2';
+                  } else if (verse.id == 7) {
+                    final char = verse.codeV2.characters.toList();
+                    final char1 = verse.codeV2.split(char[6]).first;
+                    final char2 = verse.codeV2.split(char[4]).last;
+                    final char3 = char2.split(char[14]).first;
+                    final char4 = char2.split(char[12]).last;
+                    codeV2 = '$char1\n$char3\n$char4';
+                  }
+                  return TextSpan(
+                    // text: verse.codeV2,
+                    text: codeV2,
+                    locale: const Locale('ar'),
+                    style: const TextStyle(
+                      // fontFamily: FontFamily.normalV12,
+                      fontFamily: FontFamily.tajweedV12,
+                      package: 'mq_app_ui',
+                      fontSize: 28,
+                      height: 1.8,
+                      color: Colors.black,
+                    ),
+                  );
+                }).toList(),
               ),
             ),
-          );
-        },
+          ),
+        ),
       ),
+      // body: PageView.builder(
+      //   itemCount: 10,
+      //   scrollDirection: Axis.vertical,
+      //   itemBuilder: (context, index) {
+      //     return SingleChildScrollView(
+      //       padding: const EdgeInsets.all(20),
+      //       child: Center(
+      //         child: Directionality(
+      //           textDirection: TextDirection.rtl,
+      //           child: RichText(
+      //             textAlign: TextAlign.center,
+      //             text: TextSpan(
+      //               children: _quranExModel.verses.map((verse) {
+      //                 var codeV2 = verse.codeV2;
+      //                 if (verse.id == 1) {
+      //                   codeV2 = '${verse.codeV2}\n';
+      //                 } else if (verse.id == 2) {
+      //                   codeV2 = '${verse.codeV2}\n';
+      //                 } else if (verse.id == 4) {
+      //                   codeV2 = '${verse.codeV2}\n';
+      //                 } else if (verse.id == 6) {
+      //                   final char = verse.codeV2.characters.first;
+      //                   final char2 = verse.codeV2.split(char).last;
+      //                   codeV2 = '$char\n$char2';
+      //                 } else if (verse.id == 7) {
+      //                   final char = verse.codeV2.characters.toList();
+      //                   final char1 = verse.codeV2.split(char[6]).first;
+      //                   final char2 = verse.codeV2.split(char[4]).last;
+      //                   final char3 = char2.split(char[14]).first;
+      //                   final char4 = char2.split(char[12]).last;
+      //                   codeV2 = '$char1\n$char3\n$char4';
+      //                 }
+      //                 return TextSpan(
+      //                   text: codeV2,
+      //                   locale: const Locale('ar'),
+      //                   style: const TextStyle(
+      //                     // fontFamily: FontFamily.page1V2,
+      //                     fontFamily: FontFamily.page1Woff2,
+      //                     package: 'mq_app_ui',
+      //                     fontSize: 28,
+      //                     height: 1.8,
+      //                     color: Colors.black,
+      //                   ),
+      //                 );
+      //               }).toList(),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
       // body: SingleChildScrollView(
       //   padding: const EdgeInsets.all(20),
       //   child: Center(
