0497c827daec036963df48b074903243651e00b8
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-02-14 23:37:53 +0600

    Removed unused code

--- packages/mq_quran_client/lib/constants/quran_client_constants.dart
+++ packages/mq_quran_client/lib/constants/quran_client_constants.dart
@@ -1,7 +1,5 @@
 abstract interface class QuranClientConstants {
-  static String getNormalUrl(String path) =>
-      'https://media.githubusercontent.com/media/Eldar2021/eldar2021.github.io/refs/heads/main/my-quran/quran_data_v2/normal_fonts/$path.woff2?download=true';
+  static String getNormalUrl(String path) => 'https://myquran.life/media/fonts/normal_fonts/$path.woff2';
 
-  static String getTajweedUrl(String path) =>
-      'https://media.githubusercontent.com/media/Eldar2021/eldar2021.github.io/refs/heads/main/my-quran/quran_data_v2/tajweed_fonts/$path.woff2?download=true';
+  static String getTajweedUrl(String path) => 'https://myquran.life/media/fonts/tajweed_fonts/$path.woff2';
 }
