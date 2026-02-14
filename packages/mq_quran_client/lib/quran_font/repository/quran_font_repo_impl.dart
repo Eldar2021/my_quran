98bf229a58fc964f71395aa8d75a7ef3b26699a6
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-02-15 00:21:11 +0600

    Refactored QuranFontRepository

--- packages/mq_quran_client/lib/quran_font/repository/quran_font_repo_impl.dart
+++ packages/mq_quran_client/lib/quran_font/repository/quran_font_repo_impl.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:mq_quran_client/mq_quran_client.dart';
 
 class QuranFontRepoImpl implements QuranFontRepository {
@@ -7,18 +5,18 @@ class QuranFontRepoImpl implements QuranFontRepository {
 
   final QuranFontSource source;
 
-  final _cache = <int, QuranFontModel>{};
+  final _cache = <int, Future<QuranFontModel>>{};
 
   @override
   Future<QuranFontModel> getFontByPage(int pageNumber) async {
     if (_cache.containsKey(pageNumber)) return _cache[pageNumber]!;
-    try {
-      final font = await source.getFontByPage(pageNumber);
-      _cache[pageNumber] = font;
-      return font;
-    } catch (e) {
-      log('Get Font Error: $e');
-      rethrow;
-    }
+
+    final future = source.getFontByPage(pageNumber).catchError((Object error) {
+      _cache.remove(pageNumber);
+      throw error;
+    });
+
+    _cache[pageNumber] = future;
+    return future;
   }
 }
--- packages/mq_quran_client/lib/quran_font/source/quran_font_remote_source.dart
+++ packages/mq_quran_client/lib/quran_font/source/quran_font_remote_source.dart
@@ -1,16 +1,16 @@
 import 'dart:developer';
 import 'dart:io';
 
-import 'package:meta/meta.dart';
 import 'package:mq_quran_client/mq_quran_client.dart';
 import 'package:mq_remote_client/mq_remote_client.dart';
 import 'package:path_provider/path_provider.dart';
 
-@immutable
-final class QuranFontRemoteSource implements QuranFontSource {
-  const QuranFontRemoteSource(this.client);
+class QuranFontRemoteSource implements QuranFontSource {
+  QuranFontRemoteSource(this.client);
 
   final MqRemoteClient client;
+
+  Directory? _appDir;
 
   @override
   Future<QuranFontModel> getFontByPage(int pageNumber) async {
@@ -33,15 +33,14 @@ final class QuranFontRemoteSource implements QuranFontSource {
   Future<File> _getFontByPage(int pageNumber, bool isTajweed) async {
     final fileName = pageNumber.toString().padLeft(3, '0');
     final dirName = isTajweed ? 'tajweed_fonts' : 'normal_fonts';
+    final appDir = await _getAppDir();
 
-    final appDir = await getApplicationDocumentsDirectory();
     final fontDir = Directory('${appDir.path}/quran_fonts/$dirName');
-
-    if (!fontDir.existsSync()) {
-      await fontDir.create(recursive: true);
-    }
+    if (!fontDir.existsSync()) await fontDir.create(recursive: true);
 
     final file = File('${fontDir.path}/$fileName.woff2');
+    final tempFile = File('${fontDir.path}/$fileName.temp');
+
     if (file.existsSync()) return file;
 
     try {
@@ -49,12 +48,18 @@ final class QuranFontRemoteSource implements QuranFontSource {
           ? QuranClientConstants.getTajweedUrl(fileName)
           : QuranClientConstants.getNormalUrl(fileName);
 
-      await client.download(url, file.path);
-      return file;
+      await client.download(url, tempFile.path);
+      return await tempFile.rename(file.path);
     } catch (e) {
-      if (file.existsSync()) await file.delete();
+      if (tempFile.existsSync()) await tempFile.delete();
       log('Download Error: $e');
       rethrow;
     }
+  }
+
+  Future<Directory> _getAppDir() async {
+    if (_appDir != null) return _appDir!;
+    _appDir = await getApplicationDocumentsDirectory();
+    return _appDir!;
   }
 }
