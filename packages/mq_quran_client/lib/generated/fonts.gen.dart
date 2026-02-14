b2a32010f2740f22fbdb76b299124ef3c1a3aabe
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-02-14 15:18:34 +0600

    Added Quran client assets

---
+++ packages/mq_quran_client/lib/generated/fonts.gen.dart
@@ -0,0 +1,19 @@
+// dart format width=120
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class QuranFontFamily {
+  QuranFontFamily._();
+
+  /// Font family: surah_names
+  static const String surahNames = 'surah_names';
+
+  /// Font family: uthmanic_hafs
+  static const String uthmanicHafs = 'uthmanic_hafs';
+}
