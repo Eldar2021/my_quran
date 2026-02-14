2e5c891ff85bde32e71ffcb7ac028825cd6e2a6e
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-02-14 13:46:10 +0600

    Added getQuranByPages

--- packages/functions/lib/quran_data_func/quran_texts.dart
+++ packages/functions/lib/quran_data_func/quran_texts.dart
@@ -7,8 +7,6 @@ import 'package:path/path.dart' as p;
 
 // https://api-docs.quran.foundation/docs/tutorials/fonts/font-rendering
 
-const _token = '..';
-
 const JsonEncoder encoder = JsonEncoder.withIndent('  ');
 
 Future<void> main() async {
@@ -25,34 +23,82 @@ Future<void> getQuranByPages() async {
     await dir.create(recursive: true);
   }
 
-  for (var i = 1; i <= 2; i++) {
+  for (var i = 1; i <= 604; i++) {
     final fileName = '${i.toString().padLeft(3, '0')}.json';
     final filePath = p.join(dir.path, fileName);
 
     try {
-      log('Downloading page $i...');
+      log('Downloading juz $i...');
       final response = await dio.get<Map<String, dynamic>>(
-        'https://apis-prelive.quran.foundation/content/api/v4/quran/verses/code_v2?page_number=$i',
+        'https://apis-prelive.quran.foundation/content/api/v4/verses/by_page/$i?fields=text_imlaei,text_uthmani,code_v2,chapter_id',
         options: Options(
           headers: {
             'Accept': 'application/json',
             'x-auth-token': _token,
-            'x-client-id': '89de0e22-e43f-4f39-8fe8-6d6aee6ca438',
+            'x-client-id': _clientId,
           },
         ),
       );
+      final verses = response.data?['verses'] as List<dynamic>;
+      final pagination = response.data?['pagination'] as Map<String, dynamic>?;
 
-      if (response.data != null) {
+      if (pagination?['total_pages'] == 1 &&
+          pagination?['current_page'] == 1 &&
+          pagination?['next_page'] == null &&
+          verses.isNotEmpty) {
+        log('Everything is OK');
         final file = File(filePath);
-        await file.writeAsString(encoder.convert(response.data));
+        await file.writeAsString(encoder.convert(verses));
         log('Saved: $fileName');
+      } else {
+        log('Something is wrong for page $i');
       }
     } on Object catch (e) {
       log('Error downloading page $i: $e');
     }
   }
+
   log('Tüm sayfalar indirildi.');
 }
+
+// Future<void> getQuranByPages() async {
+//   final dio = Dio();
+
+//   final currentProjectPath = p.join(Directory.current.path, 'quran_data', 'quran_pages');
+//   final dir = Directory(currentProjectPath);
+
+//   if (!dir.existsSync()) {
+//     await dir.create(recursive: true);
+//   }
+
+//   for (var i = 1; i <= 2; i++) {
+//     final fileName = '${i.toString().padLeft(3, '0')}.json';
+//     final filePath = p.join(dir.path, fileName);
+
+//     try {
+//       log('Downloading page $i...');
+//       final response = await dio.get<Map<String, dynamic>>(
+//         'https://apis-prelive.quran.foundation/content/api/v4/quran/verses/code_v2?page_number=$i',
+//         options: Options(
+//           headers: {
+//             'Accept': 'application/json',
+//             'x-auth-token': _token,
+//             'x-client-id': '89de0e22-e43f-4f39-8fe8-6d6aee6ca438',
+//           },
+//         ),
+//       );
+
+//       if (response.data != null) {
+//         final file = File(filePath);
+//         await file.writeAsString(encoder.convert(response.data));
+//         log('Saved: $fileName');
+//       }
+//     } on Object catch (e) {
+//       log('Error downloading page $i: $e');
+//     }
+//   }
+//   log('Tüm sayfalar indirildi.');
+// }
 
 // Future<void> getQuranByJuz() async {
 //   final dio = Dio();
