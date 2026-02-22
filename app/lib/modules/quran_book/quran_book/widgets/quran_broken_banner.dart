44d210f2d4861d793c617676d3814ba0f8b385c8
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-02-22 19:53:36 +0600

    feat: Add localized font error message and display it in the Quran broken banner.

--- app/lib/modules/quran_book/quran_book/widgets/quran_broken_banner.dart
+++ app/lib/modules/quran_book/quran_book/widgets/quran_broken_banner.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:my_quran/l10n/l10.dart';
 
 class QuranBrokenBanner extends StatelessWidget {
   const QuranBrokenBanner({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
       child: DecoratedBox(
         decoration: BoxDecoration(
-          border: Border.all(color: Theme.of(context).colorScheme.error),
+          border: Border.all(color: colorScheme.error),
           borderRadius: BorderRadius.circular(10),
         ),
         child: ListTile(
           leading: Icon(
             Icons.error_outline,
-            color: Theme.of(context).colorScheme.error,
+            color: colorScheme.error,
           ),
           title: Text(
-            "Bu sayfanın özel font yapısında bir hata oluştu. Kur'an'ı doğru okuyabilmeniz için geçici olarak standart Uthmani fontu gösterilmektedir. En kısa sürede düzeltilecektir.",
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).colorScheme.error,
-            ),
+            context.l10n.fontErrorMessage,
+            style: textTheme.bodyMedium?.copyWith(color: colorScheme.error),
           ),
         ),
       ),
