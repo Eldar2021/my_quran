2442f94b272b97557b999e41c487a99b9c97b88b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-03-08 03:50:26 +0600

    refactor: Enhance screenshot error handling and add required imports in `WidgetTester` extensions.

--- app/integration_test/extensions/tester_extensions.dart
+++ app/integration_test/extensions/tester_extensions.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 extension TesterExtensions on WidgetTester {
   Future<void> waitFor(Finder finder) async {
@@ -9,8 +11,6 @@ extension TesterExtensions on WidgetTester {
   Future<void> takeScreenshot(String name) async {
     await pumpAndSettle();
 
-    // In order for takeScreenshot to work on Android/iOS natively,
-    // we need to call the binding's takeScreenshot method.
     final binding = IntegrationTestWidgetsFlutterBinding.instance;
     await binding.convertFlutterSurfaceToImage();
     await pumpAndSettle();
@@ -18,7 +18,7 @@ extension TesterExtensions on WidgetTester {
     try {
       await binding.takeScreenshot(name);
       debugPrint('Screenshot captured: $name');
-    } catch (e) {
+    } on Object catch (e) {
       debugPrint('Failed to capture screenshot $name: $e');
     }
   }
