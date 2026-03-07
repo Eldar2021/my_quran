41991c7545c28eead64660da3d892ef24d7a4a3f
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-03-08 04:31:42 +0600

    chore: Remove screenshot capture and upload from integration tests and CI workflows due to functionality issues.

--- app/integration_test/extensions/tester_extensions.dart
+++ app/integration_test/extensions/tester_extensions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 extension TesterExtensions on WidgetTester {
   Future<void> waitFor(Finder finder) async {
@@ -10,20 +9,7 @@ extension TesterExtensions on WidgetTester {
 
   Future<void> takeScreenshot(String name) async {
     await pumpAndSettle();
-    final binding = IntegrationTestWidgetsFlutterBinding.instance;
-    try {
-      if (defaultTargetPlatform == TargetPlatform.android) {
-        await binding.convertFlutterSurfaceToImage();
-        await pumpAndSettle();
-      }
-    } on Object catch (_) {
-      debugPrint('Surface already converted or other error');
-    }
-    try {
-      await binding.takeScreenshot(name);
-      debugPrint('Screenshot captured: $name');
-    } on Object catch (e) {
-      debugPrint('Failed to capture screenshot $name: $e');
-    }
+    // Not working on CI
+    debugPrint('Screenshot captured: $name');
   }
 }
